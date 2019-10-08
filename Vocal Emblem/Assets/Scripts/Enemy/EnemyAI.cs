8d406594dd1172542d97df21c3519c740d671404
JordanG <26260@ma-web.nl>
2019-10-08 15:57:23 +0200

    Enemy moves to target

--- Vocal Emblem/Assets/Scripts/Enemy/EnemyAI.cs
+++ Vocal Emblem/Assets/Scripts/Enemy/EnemyAI.cs
@@ -30,11 +30,11 @@ public class EnemyAI : MonoBehaviour
     {
         if (stats.typeMovement == Stats.movementType.FLIER)
         {
-            tileData.RevealTiles(Mathf.RoundToInt(transform.position.x - 0.5f), Mathf.RoundToInt(transform.position.y - 0.5f), Mathf.RoundToInt(stats.mov), stats.equippedWeapon.range, true, !current, 2);
+            tileData.RevealTiles(Mathf.RoundToInt(transform.position.x - 0.5f), Mathf.RoundToInt(transform.position.y - 0.5f), Mathf.RoundToInt(stats.mov), stats.equippedWeapon.range, true, true, 2);
         }
         else
         {
-            tileData.RevealTiles(Mathf.RoundToInt(transform.position.x - 0.5f), Mathf.RoundToInt(transform.position.y - 0.5f), Mathf.RoundToInt(stats.mov), stats.equippedWeapon.range, false, !current, 2);
+            tileData.RevealTiles(Mathf.RoundToInt(transform.position.x - 0.5f), Mathf.RoundToInt(transform.position.y - 0.5f), Mathf.RoundToInt(stats.mov), stats.equippedWeapon.range, false, true, 2);
         }
     }
 
@@ -51,58 +51,79 @@ public class EnemyAI : MonoBehaviour
 
             target = GetClosestPlayer(tileData.players);
 
-            WalkingTowardsTarget(x, y);
+            bool facingUp = false;
+            bool facingDown = false;
+            bool facingLeft = false;
+            bool facingRight = false;
+
+            WalkingTowardsTarget(x, y, facingUp, facingDown, facingLeft, facingRight);
         }        
     }
 
-    private void WalkingTowardsTarget(int x, int y)
+    private void WalkingTowardsTarget(int x, int y, bool facingUp, bool facingDown, bool facingLeft, bool facingRight)
     {
         float distanceTop = 99;
         float distanceBottom = 99;
         float distanceLeft = 99;
         float distanceRight = 99;
+        
 
-        bool facingUp = false;
-        bool facingDown = false;
-        bool facingLeft = false;
-        bool facingRight = false;
-
-        if (y - 1 > -1 && tileData.rowsMovement[-y - 1].transform.GetChild(x).CompareTag("MoveTile"))
+        if (-y - 1 > -1 && tileData.rowsMovement[-y - 1].transform.GetChild(x).CompareTag("MoveTile"))
         {
-            distanceTop = Mathf.Abs(transform.position.x - target.position.x) + Mathf.Abs((transform.position.y - 1) - target.position.y);
+            distanceTop = Mathf.Abs(transform.position.x - target.position.x) + Mathf.Abs((transform.position.y + 1) - target.position.y);
+            Debug.Log("top " + distanceTop);
         }
-        if (y + 1 < tileData.rowsMovement.Count && tileData.rowsMovement[-y + 1].transform.GetChild(x).CompareTag("MoveTile"))
+        if (-y + 1 < tileData.rowsMovement.Count && tileData.rowsMovement[-y + 1].transform.GetChild(x).CompareTag("MoveTile"))
         {
-            distanceBottom = Mathf.Abs(transform.position.x - target.position.x) + Mathf.Abs((transform.position.y + 1) - target.position.y);
+            distanceBottom = Mathf.Abs(transform.position.x - target.position.x) + Mathf.Abs((transform.position.y - 1) - target.position.y);
+            Debug.Log("bottom " + distanceBottom);
         }
         if (x - 1 > -1 && tileData.rowsMovement[-y].transform.GetChild(x - 1).CompareTag("MoveTile"))
         {
             distanceLeft = Mathf.Abs((transform.position.x - 1) - target.position.x) + Mathf.Abs(transform.position.y - target.position.y);
+            Debug.Log("left " + distanceLeft);
         }
         if (x + 1 < tileData.rowsMovement[-y].transform.childCount && tileData.rowsMovement[-y].transform.GetChild(x - 1).CompareTag("MoveTile"))
         {
             distanceRight = Mathf.Abs((transform.position.x + 1) - target.position.x) + Mathf.Abs(transform.position.y - target.position.y);
+            Debug.Log("right " + distanceRight);
         }        
 
-        if (distanceTop < distanceBottom && distanceTop < distanceLeft && distanceTop < distanceRight && !facingDown)
-        {
-            transform.position = new Vector2(transform.position.x, transform.position.y - 1);
-            facingUp = true;
-        }
-        if (distanceBottom < distanceTop && distanceBottom < distanceLeft && distanceBottom < distanceRight && !facingUp)
+        if (distanceTop <= distanceBottom && distanceTop <= distanceLeft && distanceTop <= distanceRight && !facingDown)
         {
             transform.position = new Vector2(transform.position.x, transform.position.y + 1);
-            facingDown = true;
+            facingUp = true;
+            facingDown = false;
+            facingLeft = false;
+            facingRight = false;
+            WalkingTowardsTarget(x, y - 1, facingUp, facingDown, facingLeft, facingRight);
         }
-        if (distanceLeft < distanceBottom && distanceLeft < distanceTop && distanceLeft < distanceRight && !facingRight)
+        else if (distanceBottom < distanceTop && distanceBottom <= distanceLeft && distanceBottom <= distanceRight && !facingUp)
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y - 1);
+            facingUp = false;
+            facingDown = true;
+            facingLeft = false;
+            facingRight = false;
+            WalkingTowardsTarget(x, y + 1, facingUp, facingDown, facingLeft, facingRight);
+        }
+        else if (distanceLeft < distanceBottom && distanceLeft < distanceTop && distanceLeft <= distanceRight && !facingRight)
         {
             transform.position = new Vector2(transform.position.x - 1, transform.position.y);
+            facingUp = false;
+            facingDown = false;
             facingLeft = true;
+            facingRight = false;
+            WalkingTowardsTarget(x - 1, y, facingUp, facingDown, facingLeft, facingRight);
         }
-        if (distanceRight < distanceBottom && distanceRight < distanceLeft && distanceRight < distanceTop && !facingLeft)
+        else if (distanceRight < distanceBottom && distanceRight < distanceLeft && distanceRight < distanceTop && !facingLeft)
         {
             transform.position = new Vector2(transform.position.x + 1, transform.position.y);
+            facingUp = false;
+            facingDown = false;
+            facingLeft = false;
             facingRight = true;
+            WalkingTowardsTarget(x + 1, y, facingUp, facingDown, facingLeft, facingRight);
         }
     }
 
